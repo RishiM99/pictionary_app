fba61a71d9f94e5830941b0f807d4922e96f34c2
Rishi Maheshwari <rishim@berkeley.edu>
2024-08-19 23:59:19 -0700

    Create socket.io server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const path = require('path');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+const server = createServer(app)
+
+// socket.io Server
+const io = new Server(server)
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -17,6 +23,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
