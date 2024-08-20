267e956e9d7e0b17be1dd96ee039de2957ac5cf1
Rishi Maheshwari <rishim@berkeley.edu>
2024-08-20 00:08:19 -0700

    Change window URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 const { io } = require('socket.io-client');
 
-const URL = window.location;
+const URL = "http://pictionary-app-dev.us-west-2.elasticbeanstalk.com/";
 
 export const socket = io(URL);
