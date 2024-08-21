755a331fa9aae521bef215db5034d8bdabe0ca30
Rishi Maheshwari <rishim@berkeley.edu>
2024-08-20 18:35:34 -0700

    build

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,26 @@ import "./App.css";
 
 
 function App() {
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>{!data ? "Loading..." : data}</p>
+  //     </header>
+  //   </div>
+  // );
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      <p>{"Loading..."}</p>
     </div>
   );
 }
