2ed4dfe0dad53f4f7b825ef0264881973b528acb
Rishi Maheshwari <rishim@berkeley.edu>
2024-09-24 18:26:03 -0700

    Create clear canvas button

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -24,6 +24,7 @@ export default function Canvas({ roomId, roomNameHeaderHeight, currentPlayersSid
     const colorPickerRef = useRef<HTMLDivElement>(null);
     const drawStrokePickerRef = useRef<HTMLDivElement>(null);
     const eraseStrokePickerRef = useRef<HTMLDivElement>(null);
+    const clearCanvasButtonRef = useRef<HTMLDivElement>(null);
 
 
     const [selectedPaletteOption, setSelectedPaletteOption] = useState<PaletteOption>(PaletteOption.Pen);
@@ -110,27 +111,50 @@ export default function Canvas({ roomId, roomNameHeaderHeight, currentPlayersSid
                 <div className={cursorClass} ref={cursorRef} > </div>
                 < canvas className="drawing-canvas" ref={drawingCanvasRef} />
                 <div className="palette" ref={paletteRef} >
-                    <div className={selectedPaletteOption === PaletteOption.Pen ? "draw-icon-background-selected" : "draw-icon-background-unselected"} ref={openDrawStrokePickerButtonRef} onClick={penButtonOnClick} >
-                        <svg viewBox="0 0 512 512" className="draw-icon" height="70%" width="70%" xmlns="http://www.w3.org/2000/svg" >
-                            <path d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 
-                            49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 
-                            30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 
-                            339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 
-                            5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path>
-                        </svg>
+                    <div>
+                        <div className={selectedPaletteOption === PaletteOption.Pen ? "draw-icon-background-selected" : "draw-icon-background-unselected"} ref={openDrawStrokePickerButtonRef} onClick={penButtonOnClick} >
+                            <svg viewBox="0 0 512 512" className="draw-icon" height="70%" width="70%" xmlns="http://www.w3.org/2000/svg" >
+                                <path d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 
+                                49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 
+                                30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 
+                                339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 
+                                5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"></path>
+                            </svg>
+                        </div>
+                        <p className="palette-icon-description"> Draw </p>
                     </div>
 
-                    < div className={selectedPaletteOption === PaletteOption.Eraser ? "erase-icon-background-selected" : "erase-icon-background-unselected"} ref={openEraseStrokePickerButtonRef} onClick={eraserButtonOnClick} >
-                        <svg viewBox="0 0 512 512" className="erase-icon" height="70%" width="70%" xmlns="http://www.w3.org/2000/svg" >
-                            <path d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883 
-                            0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373 
-                            12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373 
-                            416H150.628l-80-80 124.686-124.686z"></path>
-                        </svg>
+
+                    <div>
+                        < div className={selectedPaletteOption === PaletteOption.Eraser ? "erase-icon-background-selected" : "erase-icon-background-unselected"} ref={openEraseStrokePickerButtonRef} onClick={eraserButtonOnClick} >
+                            <svg viewBox="0 0 512 512" className="erase-icon" height="70%" width="70%" xmlns="http://www.w3.org/2000/svg" >
+                                <path d="M497.941 273.941c18.745-18.745 18.745-49.137 0-67.882l-160-160c-18.745-18.745-49.136-18.746-67.883 
+                                0l-256 256c-18.745 18.745-18.745 49.137 0 67.882l96 96A48.004 48.004 0 0 0 144 480h356c6.627 0 12-5.373 
+                                12-12v-40c0-6.627-5.373-12-12-12H355.883l142.058-142.059zm-302.627-62.627l137.373 137.373L265.373 
+                                416H150.628l-80-80 124.686-124.686z"></path>
+                            </svg>
+                        </div>
+                        <p className='palette-icon-description'> Erase </p>
                     </div>
-                    < div className="color-picker-button-border" ref={openColorPickerButtonRef} onClick={colorPickerButtonOnClick} >
-                        <div className={convertColorToString(currentColor)} style={{ "height": "25px", "width": "25px" }} />
+
+
+                    <div>
+                        < div className="clear-canvas-background" ref={clearCanvasButtonRef} >
+                            <svg viewBox="0 0 512 512" className="clear-canvas-icon" height="70%" width="70%" xmlns="http://www.w3.org/2000/svg" >
+                                <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path>
+                            </svg>
+                        </div>
+                        <p className='palette-icon-description'> Clear </p>
                     </div>
+
+
+                    <div>
+                        < div className="color-picker-button-border" ref={openColorPickerButtonRef} onClick={colorPickerButtonOnClick} >
+                            <div className={convertColorToString(currentColor)} style={{ "height": "25px", "width": "25px" }} />
+                        </div>
+                        <p > </p>
+                    </div>
+
                 </div>
                 {showColorPicker && <ColorPicker ref={colorPickerRef} />}
                 {showDrawStrokePicker && <DrawStrokePicker ref={drawStrokePickerRef} />}
