dea07b7eb1e0d9e71cc1388e9bbd0127f6e75439
Rishi Maheshwari <rishi.maheshwari930@gmail.com>
2024-10-08 22:34:22 -0700

    compiled javascript for server

--- server/compiled_javascript/client/src/common/SocketEvents.d.ts
+++ server/compiled_javascript/client/src/common/SocketEvents.d.ts
@@ -15,15 +15,24 @@ export type BroadcastDrawingPathsDiffType = {
     width: number;
     height: number;
 };
+export type RoomState = {
+    paths: UUIDandSerializedPath[];
+    width: number;
+    height: number;
+};
 export interface ServerToClientEvents {
     listOfRoomsAndMembers: (listOfRoomsAndMembers: RoomAndMembers[]) => void;
     nameOfNewRoom: (roomId: string) => void;
     broadcastDrawingPathsDiff: (msg: BroadcastDrawingPathsDiffType) => void;
+    sendRoomStateUponJoining: (msg: RoomState) => void;
+    requestCurrentRoomState: () => void;
 }
 export interface ClientToServerEvents {
     createRoom: (roomName: string) => void;
     joinRoom: (roomName: string) => void;
     getListOfRoomsAndMembers: () => void;
     drawingPathsDiffFromClient: (msg: DrawingPathsDiffFromClientType) => void;
+    getRoomStateUponJoining: () => void;
+    sendCurrentRoomState: (roomState: RoomState) => void;
 }
 //# sourceMappingURL=SocketEvents.d.ts.map
