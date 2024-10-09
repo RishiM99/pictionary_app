7f5bc059f6bfe288fe6a8972cae0a1bb2e63cdcf
Rishi Maheshwari <rishim@berkeley.edu>
2024-10-08 22:19:09 -0700

    Chagne sql dump

--- pictionary_app_db.sql
+++ pictionary_app_db.sql
@@ -16,28 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: rishimaheshwari; Type: DATABASE; Schema: -; Owner: rishimaheshwari
---
-
-CREATE DATABASE rishimaheshwari WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.UTF-8';
-
-
-ALTER DATABASE rishimaheshwari OWNER TO rishimaheshwari;
-
-\connect rishimaheshwari
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
